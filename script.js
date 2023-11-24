a8c110761043d20ba01fb3d1bd2cfce351a88802
Rido <151827423+rido30@users.noreply.github.com>
2023-11-24 15:45:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gcpscian", "Learners", "HSC-25 b.",],
+        strings: ["Gcpscian", "Class 11", "Buissness G.",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
